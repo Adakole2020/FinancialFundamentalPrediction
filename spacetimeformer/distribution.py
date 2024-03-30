597d8b060131b529c225ad3ee3ab7378855ece61
Adakole Ebute <78976996+Adakole2020@users.noreply.github.com>
2024-03-30 04:25:30 -0700

    Add numerically stable cdf

--- spacetimeformer/distribution.py
+++ spacetimeformer/distribution.py
@@ -5,7 +5,36 @@ import torch
 from torch.distributions import constraints
 from torch.distributions.distribution import Distribution
 from torch.distributions.utils import broadcast_all
+import numpy as np
 
+# Numerically stable and accurate implementation of the natural logarithm
+# of the cumulative distribution function (CDF) for the standard
+# Normal/Gaussian distribution in PyTorch. https://gist.github.com/chausies/011df759f167b17b5278264454fff379
+
+
+def norm_cdf(x):
+    return (1 + torch.erf(x/math.sqrt(2)))/2
+
+def log_norm_cdf_helper(x):
+    a = 0.344
+    b = 5.334
+    return ((1 - a)*x + a*x**2+b).sqrt()
+
+def log_norm_cdf(x):
+    thresh = 3
+    out = x*0
+    l = x<-thresh
+    g = x>thresh
+    m = torch.logical_and(x>=-thresh, x<=thresh)
+    out[m] = norm_cdf(x[m]).log()
+    out[l] = -(
+        (x[l]**2 + np.log(2*np.pi))/2 + 
+        log_norm_cdf_helper(-x[l]).log()
+        )
+    out[g] = torch.log1p(-
+        (-x[g]**2/2).exp()/np.sqrt(2*np.pi)/log_norm_cdf_helper(x[g])
+        )
+    return out
 
 class SkewNormal(Distribution):
     r"""
@@ -31,58 +60,28 @@ class SkewNormal(Distribution):
 
     @property
     def mean(self):
-        """
-        Computes the mean of the skew normal distribution.
-
-        Returns:
-            Tensor: The mean of the distribution.
-        """
         delta = self.alpha / torch.sqrt(1 + self.alpha.pow(2))
         return self.loc + self.scale * delta * math.sqrt(2 / math.pi)
 
     @property
-    def stddev(self):
-        """
-        Computes the standard deviation of the skew normal distribution.
+    def mode(self):
+        return self.loc
 
-        Returns:
-            Tensor: The standard deviation of the distribution.
-        """
+    @property
+    def stddev(self):
         return torch.sqrt(self.variance)
     
     @property
     def variance(self):
-        """
-        Computes the variance of the skew normal distribution.
-
-        Returns:
-            Tensor: The variance of the distribution.
-        """
         delta = self.alpha / torch.sqrt(1 + self.alpha.pow(2))
         return self.scale.pow(2) * (1 - 2 * delta.pow(2) / math.pi)
     
     @property
     def skewness(self):
-        """
-        Computes the skewness of the skew normal distribution.
-
-        Returns:
-            Tensor: The skewness of the distribution.
-        """
         delta = self.alpha / torch.sqrt(1 + self.alpha.pow(2))
         return (4 - math.pi) / 2 * (delta * torch.sqrt(2 / math.pi)).pow(3) / (1 - 2 * delta.pow(2) / math.pi).pow(1.5)
 
     def __init__(self, loc, scale, alpha, validate_args=None):
-        """
-        Initializes a skew normal distribution.
-
-        Args:
-            loc (float or Tensor): mean of the distribution (often referred to as mu)
-            scale (float or Tensor): standard deviation of the distribution
-                (often referred to as sigma)
-            alpha (float or Tensor): skewness parameter of the distribution
-            validate_args (bool, optional): Whether to validate input arguments. Default: None
-        """
         self.loc, self.scale, self.alpha = broadcast_all(loc, scale, alpha)
         if isinstance(loc, Number) and isinstance(scale, Number) and isinstance(alpha, Number):
             batch_shape = torch.Size()
@@ -91,16 +90,6 @@ class SkewNormal(Distribution):
         super().__init__(batch_shape, validate_args=validate_args)
 
     def expand(self, batch_shape, _instance=None):
-        """
-        Returns a new SkewNormal instance with expanded batch shape.
-
-        Args:
-            batch_shape (torch.Size): The desired expanded batch shape.
-            _instance (SkewNormal, optional): The instance to be expanded. Default: None
-
-        Returns:
-            SkewNormal: The expanded SkewNormal instance.
-        """
         new = self._get_checked_instance(SkewNormal, _instance)
         batch_shape = torch.Size(batch_shape)
         new.loc = self.loc.expand(batch_shape)
@@ -111,15 +100,6 @@ class SkewNormal(Distribution):
         return new
 
     def sample(self, sample_shape=torch.Size()): 
-        """
-        Generates a sample from the skew normal distribution.
-
-        Args:
-            sample_shape (torch.Size, optional): The desired shape of the sample. Default: torch.Size()
-
-        Returns:
-            Tensor: A sample from the skew normal distribution.
-        """
         # https://stats.stackexchange.com/questions/316314/sampling-from-skew-normal-distribution
         shape = self._extended_shape(sample_shape)
         with torch.no_grad():
@@ -132,15 +112,6 @@ class SkewNormal(Distribution):
             return self.loc + self.scale * Z / torch.sqrt(1 + self.alpha.pow(2))
 
     def rsample(self, sample_shape=torch.Size()):
-        """
-        Generates a reparameterized sample from the skew normal distribution.
-
-        Args:
-            sample_shape (torch.Size, optional): The desired shape of the sample. Default: torch.Size()
-
-        Returns:
-            Tensor: A reparameterized sample from the skew normal distribution.
-        """
         shape = self._extended_shape(sample_shape)
         U = torch.randn(shape, dtype=self.loc.dtype, device=self.loc.device)
         V = torch.randn(shape, dtype=self.loc.dtype, device=self.loc.device)
@@ -151,57 +122,24 @@ class SkewNormal(Distribution):
         return self.loc + self.scale * Z / torch.sqrt(1 + self.alpha.pow(2))
 
     def log_prob(self, value):
-        """
-        Computes the log probability density/mass function (PDF/PMF) of the skew normal distribution.
-
-        Args:
-            value (Tensor): The value(s) at which to compute the log probability.
-
-        Returns:
-            Tensor: The log probability density/mass function (PDF/PMF) of the distribution evaluated at the given value(s).
-        """
         if self._validate_args:
             self._validate_sample(value)
         z = (value - self.loc) * self.scale.reciprocal()
         phi = -0.5 * z.pow(2) - math.log(math.sqrt(2 * math.pi))
-        Phi = torch.log(1 + torch.clamp(torch.erf(self.alpha * z / math.sqrt(2)), min=-0.99999))
+        Phi = log_norm_cdf(self.alpha*z)
         log_scale = (
             math.log(self.scale) if isinstance(self.scale, Real) else self.scale.log()
         )
         
-        return phi + Phi - log_scale
+        return phi + Phi - log_scale + math.log(2)
 
     def cdf(self, value):
-        """
-        Computes the cumulative distribution function (CDF) of the skew normal distribution.
-
-        Args:
-            value (Tensor): The value(s) at which to compute the CDF.
-
-        Raises:
-            NotImplementedError: The CDF is not implemented due to its complexity.
-        """
         if self._validate_args:
             self._validate_sample(value)
         raise NotImplementedError("CDF is not implemented due to its complexity.")
 
     def icdf(self, value):
-        """
-        Computes the inverse cumulative distribution function (ICDF) of the skew normal distribution.
-
-        Args:
-            value (Tensor): The value(s) at which to compute the ICDF.
-
-        Raises:
-            NotImplementedError: The ICDF is not implemented due to its complexity.
-        """
         raise NotImplementedError("Inverse CDF is not implemented due to its complexity.")
 
     def entropy(self):
-        """
-        Computes the entropy of the skew normal distribution.
-
-        Raises:
-            NotImplementedError: The entropy is not implemented due to its complexity.
-        """
         raise NotImplementedError("Entropy is not implemented due to its complexity.")
